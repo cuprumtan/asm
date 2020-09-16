ec9aad8ccbcd383dd8c26b91fb03050278d1721b
Alexandra <42344303+cuprumtan@users.noreply.github.com>
2020-09-16 07:42:29 +0300

    Refactor xor_encrypt.asm

--- xor_encrypt.asm
+++ xor_encrypt.asm
@@ -11,15 +11,15 @@ mydata db 100
 start:       mov ax,@data
              mov ds,ax
              mov bx,0
-loopstart:   cmp bx,99 			; eq for i=0..99
-             je loopend 		; if i eq 99 end loop
-             xor mydata[bx],'X' 	; encrypting
-             add mydata[bx], 50     ; readable output
-             mov dl,mydata[bx]	; write to data register
-             mov ah,2h 			; print symbol
+loopstart:   cmp bx,99 			      ;eq for i=0..99
+             je loopend 		      ;if i eq 99 end loop
+             xor mydata[bx],'X' 	;encrypting
+             add mydata[bx], 50   ;readable output
+             mov dl,mydata[bx]	  ;write to data register
+             mov ah,2h 			      ;print symbol
              int 21h
              inc bx
              jmp loopstart
-loopend:     mov ax,4c00h 		; DOS exit
+loopend:     mov ax,4c00h 		    ;DOS exit
              int 21h
 end start
